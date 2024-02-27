0ee4c79a642f5c6c42c9b63d59e07acfd857da72
AlavalapatiRupeshReddy <124611307+AlavalapatiRupeshReddy@users.noreply.github.com>
2024-02-27 15:45:02 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
+        int i;
         for (i=0; i<10; i++){
         System.out.println(MESSAGE);
         }
